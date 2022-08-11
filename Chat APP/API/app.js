c622611fa27228f8b7a68887b41e235582467a0c
Mehmet Öztürk <mehmet.ztrk2134@gmail.com>
2022-08-11 09:28:10 +0300

    comment line.

--- Chat APP/API/app.js
+++ Chat APP/API/app.js
@@ -1,26 +1,29 @@
 const path = require('path');
-const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const multer = require('multer');
 
+const express = require('express');
+const app = express();
+
+
+//routes
 const feedRoutes = require('./routes/feed');
 const authRoutes=require("./routes/auth")
 
-const app = express();
-
+//for images
+const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
  
 const fileStorage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function(req, file, cb) {  //kayıt yeri
         cb(null, 'images');
     },
-    filename: function(req, file, cb) {
+    filename: function(req, file, cb) {  //kayıt adı
         cb(null, uuidv4())
     }
 });
 
-const fileFilter = (req, file, cb) => {
+const fileFilter = (req, file, cb) => {  //gelen resimlerin tip kontrolü
   if (
     file.mimetype === 'image/png' ||
     file.mimetype === 'image/jpg' ||
@@ -34,11 +37,15 @@ const fileFilter = (req, file, cb) => {
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+
+//for images
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+
+//gelen istekler için CORS ayarlamaları.
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -49,10 +56,13 @@ app.use((req, res, next) => {
   next();
 });
 
+
+//call routes.
+
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 
-
+//oluşan hataları gönderen middleware
 app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
   const message = error.message;
@@ -60,6 +70,7 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message,data:data });
 });
 
+//mongodb ye bağlandıktan sonra 8080 portunu aç.
 mongoose
   .connect(
     'mongodb://127.0.0.1:27017/chat'
