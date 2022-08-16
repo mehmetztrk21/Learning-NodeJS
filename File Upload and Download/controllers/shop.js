882b7a707963442aa50d966016e256aaa14ada63
Mehmet Öztürk <mehmet.ztrk2134@gmail.com>
2022-08-16 14:05:46 +0300

    Add authorize for access file. (Invoice)

--- File Upload and Download/controllers/shop.js
+++ File Upload and Download/controllers/shop.js
@@ -143,15 +143,26 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  const InvoiceName = "invoice-" + orderId + ".pdf";
-  console.log(orderId);
-  const invoicePath = path.join("data", "invoices", InvoiceName);
-  fs.readFile(invoicePath, (err, data) => {
-    if (err) {
-      return next(err);
+
+  Order.findById(orderId).then(order => {
+    if (!order) {
+      return next(new Error("No order found."))
     }
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader('Content-Disposition', 'outline;filename="' + InvoiceName + '"');  //nerede açılsın ve dosya ismi ne olsun? (outline,inline olabilir.);
-    res.send(data);
+    if (order.user.userId.toString() !== req.user._id.toString()) {
+      return next(new Error("Unauthorized"));
+    }
+    
+    const InvoiceName = "invoice-" + orderId + ".pdf";
+    const invoicePath = path.join("data", "invoices", InvoiceName);
+    fs.readFile(invoicePath, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader('Content-Disposition', 'outline;filename="' + InvoiceName + '"');  //nerede açılsın ve dosya ismi ne olsun? (outline,inline olabilir.);
+      res.send(data);
+    })
   })
+    .catch(err => { next(err) });
+
 }
